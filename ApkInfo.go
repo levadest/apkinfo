3d532edd490a22307186cd5d30b5e31b2bb37a9a
lev <levakhnazarov@gmail.com>
2018-04-06 18:39:56 +0300

    r

---
+++ ApkInfo.go
@@ -0,0 +1,146 @@
+package ApkInfGo
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type ApkInfoSt struct {
+	Name               string
+	VersionCode        uint32
+	VersionName        string
+	Label              string
+	Icon               string
+	SdkVersion         uint16
+	TargetSdkVersion   uint16
+	NativeCode         []string
+	FileSize           int64
+	FilePath           string
+	Cert               ApkCertSt
+	LaunchableActivity string
+}
+
+type Conf struct {
+	aapt string
+	cert *ConfCert
+}
+
+func ApkInfo(aaptApp string) *Conf {
+	c := &Conf{aapt: aaptApp, cert: nil}
+	return c
+}
+
+func (c *Conf) CertKeyTool(keytoolApp string) *Conf {
+	app := ApkCertificate(keytoolApp)
+	c.cert = app
+	return c
+}
+
+func (c *Conf) File(apk string) *ApkInfoSt {
+	d := parse(c, apk)
+	if d != nil {
+		file, _ := os.Stat(apk)
+		d.FileSize = file.Size()
+	}
+	return d
+}
+
+func getLineSeparator() string {
+	if runtime.GOOS == "windows" {
+		return "\r\n"
+	}
+	return "\n"
+}
+
+func parse(c *Conf, apk string) *ApkInfoSt {
+	out, err := exec.Command(c.aapt, "dump", "badging", apk).Output()
+	if err != nil {
+		log.Printf("err: %q, file: %q", err, apk)
+		return nil
+	}
+	//log.Printf("apk file - %q\n", apk)
+	data := strings.Split(string(out), getLineSeparator())
+	info := ApkInfoSt{FilePath: apk}
+	for _, s := range data {
+		arr := strings.Split(s, ":")
+		if len(arr) != 2 {
+			//log.Printf("error split - %q\n", s)
+			continue
+		}
+		switch arr[0] {
+		case "package":
+			//log.Printf("package - %q\n", arr[1])
+			re := regexp.MustCompile("name='([^']+)?' versionCode='(\\d*)?' versionName='([^']+)?'")
+			packageInfo := re.FindStringSubmatch(arr[1])
+			info.Name = packageInfo[1]
+			info.VersionName = packageInfo[3]
+			versionCode, _ := strconv.ParseUint(packageInfo[2], 0, 32)
+			info.VersionCode = uint32(versionCode)
+		case "launchable-activity":
+			re := regexp.MustCompile("name='([^']+)?'")
+			launchInfo := re.FindStringSubmatch(arr[1])
+			info.LaunchableActivity = launchInfo[1]
+		case "sdkVersion":
+			//log.Printf("sdkVersion - %q\n", arr[1])
+			sdkVersion, _ := strconv.ParseUint(strings.Trim(arr[1], "'"), 0, 16)
+			info.SdkVersion = uint16(sdkVersion)
+		case "targetSdkVersion":
+			//log.Printf("targetSdkVersion - %q\n", arr[1])
+			targetSdkVersion, _ := strconv.ParseUint(strings.Trim(arr[1], "'"), 0, 16)
+			info.TargetSdkVersion = uint16(targetSdkVersion)
+		case "native-code":
+			arr[1] = strings.Replace(arr[1], "''", "", -1)
+			nativeCode := strings.Trim(strings.TrimSpace(arr[1]), "'")
+			info.NativeCode = strings.Split(nativeCode, "' '")
+			sort.Strings(info.NativeCode)
+		case "application":
+			//log.Printf("application - %q\n", arr[1])
+			re2 := regexp.MustCompile("label='([^']+)?' icon='([^']+)?'")
+			d := re2.FindStringSubmatch(arr[1])
+			info.Label = d[1]
+			info.Icon = d[2]
+		}
+	}
+	if c.cert != nil {
+		info.Cert = *c.cert.File(apk)
+	}
+	return &info
+}
+
+func (c *Conf) Folder(dirname string, recurcive bool) *[]ApkInfoSt {
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		log.Printf("err: %q", err)
+		return nil
+	}
+	//log.Printf("apk folder - %q\n", dirname)
+	infoArr := make([]ApkInfoSt, 0)
+	re := regexp.MustCompile(".*\\.apk")
+	for _, file := range files {
+		if re.MatchString(file.Name()) {
+			dir := dirname + string(os.PathSeparator) + file.Name()
+			a := parse(c, dir)
+			if a != nil {
+				a.FileSize = file.Size()
+				infoArr = append(infoArr, *a)
+			}
+		} else if file.IsDir() && recurcive {
+			dir := dirname + string(os.PathSeparator) + file.Name()
+			//log.Printf("apk subfolder - %q\n", dir)
+			arr := (c).Folder(dir, true)
+			if arr != nil {
+				for _, a := range *arr {
+					infoArr = append(infoArr, a)
+				}
+			}
+		}
+	}
+	return &infoArr
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# ApkInfGo
+Golang parser for android apk files. It extracts version information, package name, etc.
+
+##Example
+```go
+package main
+
+import "fmt"
+import "bitbucket.org/levadestroy/apkinfgo"
+
+func main() {
+	aapt := "d:\\Android\\sdk\\build-tools\\24.0.0\\aapt.exe"
+    keytool := "c:\\Program Files\\Java\\jdk1.8.0_92\\bin\\keytool.exe"
+
+    // without information on the certificate
+	//apk := ApkInfGo.ApkInfo(app).File("d:\\downloads\\file.apk")
+
+	apk := ApkInfGo.ApkInfo(app).CertKeyTool(keytool).File("d:\\downloads\\file.apk")
+	fmt.Printf("%q\n", *apk)
+
+	apks := ApkInfGo.ApkInfo(app).CertKeyTool(keytool).Folder("d:\\downloads", true)
+	if apks != nil {
+		for _, file := range *apks {
+			fmt.Printf("%q\n", file)
+		}
+		fmt.Printf("Count - %d\n", len(*apks))
+	}
+
+	cert := ApkInfGo.ApkCertificate(keytool).File("d:\\downloads\\file.apk")
+    fmt.Printf("%q\n", *cert)
+}
+```
